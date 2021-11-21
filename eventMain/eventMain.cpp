bc09fec4069adb0654c72b83a801773cabc3e957
Supperslash <32471649+Supperslash@users.noreply.github.com>
2021-11-20 21:58:40 -0600

    Event2. updated the code from the event2.
    Code is an update to include the pot in it.

--- eventMain/eventMain.cpp
+++ eventMain/eventMain.cpp
@@ -1,28 +1,40 @@
 /*
-By       MAderdash
-DATE:    11-17-1021
-Licence: BeerWare.
-Discription: This is the code for the event in Discord server.
-
-
+Blink 10 led's.
 */
-const int pin13      = 13;
+const int TEMP_PIN       = A0; // Temp analog pin.
+const int CLOCK_PIN      = 13; // Clock pin for 17 chip
+const int RESET_PIN      = 12; // Clear pin to clear the display.
 
 void setup(){
-  pinMode(pin13, OUTPUT);
+  pinMode(TEMP_PIN, INPUT);
+  pinMode(CLOCK_PIN, OUTPUT);
+  pinMode(RESET_PIN, OUTPUT);
   Serial.begin(9600);
   Serial.println("startup");
-  
 }
-void Temp(){
-Serial.println("Inside the temp function");
+//Input data gathering.
+int tempReading(){
+  int reading = analogRead(TEMP_PIN);
+  //float math = (5.0/1023.0) * reading;
+  int math = map(reading, 0, 1023, 0, 10);
+  Serial.println(math);
+  return math; // Sending the maped data.
+}
+// Drives the led's through the 4017 chip.
+void decDriver(){
+  for (int count = 0; count < tempReading(); count++){
+  digitalWrite(CLOCK_PIN, HIGH);
+  delay(1);
+  digitalWrite(CLOCK_PIN, LOW);
+  delay(10);
+  } // Below here we reset the chip.
+  digitalWrite(RESET_PIN, HIGH);
+  delay(1);
+  digitalWrite(RESET_PIN, LOW);
+  delay(1);
 }
 
 void loop(){
-  Temp();
-  digitalWrite(pin13, HIGH);
-  delay(990);
-  digitalWrite(pin13, LOW);
-  delay(10);
-  
+  decDriver();
+  //tempReading(); // for testing only.
 }
